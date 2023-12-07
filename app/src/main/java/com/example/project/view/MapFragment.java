0d1a3a424bee260abbc99cf29fd01909ae591737
jordanx2 <jordanmooney9@gmail.com>
2023-12-07 17:35:32 +0000

    case added to deal with user not granting permission

--- app/src/main/java/com/example/project/view/MapFragment.java
+++ app/src/main/java/com/example/project/view/MapFragment.java
@@ -128,18 +128,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         maps = googleMap;
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            maps.setMyLocationEnabled(true);
         }
 
-        maps.setMyLocationEnabled(true);
         maps.getUiSettings().setMyLocationButtonEnabled(false);
         FusedLocationProviderClient clientLocation = LocationServices.getFusedLocationProviderClient(getContext());
 
